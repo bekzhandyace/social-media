6644cf364786f1e7bd3eb7d6a8d4e582904643c5
Бекжан <bekzhandyace@bekzhandyace.local>
2022-11-02 14:42:50 +0600

    init

---
+++ lib/widgtes.dart/user_profile.dart
@@ -0,0 +1,173 @@
+import 'package:flutter/material.dart';
+
+class UserProfile extends StatelessWidget {
+  final List<MenuRowData> menuRow = [
+    MenuRowData(Icons.bookmark, 'Saved Mesagges'),
+    MenuRowData(Icons.call, 'Recent Calls'),
+    MenuRowData(Icons.computer, 'Devices'),
+    MenuRowData(Icons.folder, 'Chat Folders'),
+  ];
+  UserProfile();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      appBar: AppBar(
+        title: Text('Settings'),
+        centerTitle: true,
+      ),
+      body: Container(
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _UserInfo(),
+            SizedBox(height: 30),
+            _MenuWidget(menuRow: menuRow),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MenuRowData {
+  final IconData icon;
+  final String text;
+
+  MenuRowData(this.icon, this.text);
+}
+
+class _MenuWidget extends StatelessWidget {
+  final List<MenuRowData> menuRow;
+  const _MenuWidget({
+    Key? key,
+    required this.menuRow,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      width: double.infinity,
+      child: Column(
+        children: menuRow.map((data) => _MenuWidgetRow(data: data)).toList(),
+      ),
+    );
+  }
+}
+
+class _MenuWidgetRow extends StatelessWidget {
+  final MenuRowData data;
+
+  const _MenuWidgetRow({
+    Key? key,
+    required this.data,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Icon(data.icon),
+          SizedBox(width: 15),
+          Expanded(
+            child: Text(data.text),
+          ),
+          Icon(Icons.chevron_right),
+        ],
+      ),
+    );
+  }
+}
+
+class _UserInfo extends StatelessWidget {
+  const _UserInfo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      color: Colors.white,
+      child: Column(
+        children: [
+          SizedBox(height: 30),
+          _AvatarWidget(),
+          SizedBox(height: 30),
+          _UserNameWidget(),
+          SizedBox(height: 10),
+          _UserPhoneWidget(),
+          SizedBox(height: 10),
+          _UserNickNameWidget(),
+        ],
+      ),
+    );
+  }
+}
+
+class _UserNickNameWidget extends StatelessWidget {
+  const _UserNickNameWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('@Kto',
+        style: TextStyle(
+          color: Colors.grey,
+          fontSize: 17,
+        ));
+  }
+}
+
+class _UserPhoneWidget extends StatelessWidget {
+  const _UserPhoneWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('+7(144)765 87 13',
+        style: TextStyle(
+          color: Colors.grey,
+          fontSize: 15,
+        ));
+  }
+}
+
+class _UserNameWidget extends StatelessWidget {
+  const _UserNameWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'Bekzhan Dyace',
+      style: TextStyle(
+        color: Colors.black,
+        fontSize: 25,
+        fontWeight: FontWeight.w600,
+      ),
+    );
+  }
+}
+
+class _AvatarWidget extends StatelessWidget {
+  const _AvatarWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Image(
+      image: AssetImage('assets/images/three.png'),
+      width: 100,
+      height: 100,
+    );
+  }
+}
